bfe339f60d32b7469397009b4249cb20a313971b
Hamza Elhaiki <C130028975@um5.ac.ma>
2025-10-20 14:45:45 +0100

    adding others Exercices code

---
+++ Exercice 1/programme.ino
@@ -0,0 +1,13 @@
+#include <Servo.h>
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("Salam alaykoum");
+  Serial.println("Azul Fellawn");
+  Serial.println("Bonjour à tous");
+  delay(1000);
+}
