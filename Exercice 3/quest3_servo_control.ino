885683b07c0b156eb9bdd0d56712565cbf87aec5
Yassine Kerroumi <kerroumiyassin7@gmail.com>
2025-10-21 19:58:52 +0100

    bug fixes

--- Exercice 3/quest3_servo_control.ino
+++ Exercice 3/quest3_servo_control.ino
@@ -25,8 +25,6 @@ Servo servoMoteur;   ///Objet servo global pour controler le SG90
 void setup() {
   Serial.begin(9600);
   servoMoteur.attach(SERVO_PIN);
-  
-  Serial.println("Controle Servo SG90");
   Serial.println("Tapez un chiffre entre 1 et 7 pour choisir la position");
   Serial.println("Positions: 0, 30, 60, 90, 120, 150, 180 degres");
 }
@@ -41,17 +39,17 @@ void loop() {
   if (Serial.available() > 0) {
     char commande = Serial.read();  
     // Ignore les retours chariots
-    if (commande == '\r' || cmd == '\n') return;
+    if (commande == '\r' || commande == '\n') return;
 
     // Selection de la position selon la commande
     switch (commande) {
-      case '1': servoMoteur.write(0);   Serial.println("0 degres"); break;
-      case '2': servoMoteur.write(30);  Serial.println("30 degres"); break;
-      case '3': servoMoteur.write(60);  Serial.println("60 degres"); break;
-      case '4': servoMoteur.write(90);  Serial.println("90 degres"); break;
-      case '5': servoMoteur.write(120); Serial.println("120 degres"); break;
-      case '6': servoMoteur.write(150); Serial.println("150 degres"); break;
-      case '7': servoMoteur.write(180); Serial.println("180 degres"); break;
+      case '0': servoMoteur.write(0);   Serial.println("0 degres"); break;
+      case '1': servoMoteur.write(30);  Serial.println("30 degres"); break;
+      case '2': servoMoteur.write(60);  Serial.println("60 degres"); break;
+      case '3': servoMoteur.write(90);  Serial.println("90 degres"); break;
+      case '4': servoMoteur.write(120); Serial.println("120 degres"); break;
+      case '5': servoMoteur.write(150); Serial.println("150 degres"); break;
+      case '6': servoMoteur.write(180); Serial.println("180 degres"); break;
       default:
         Serial.println("Commande invalide! Tapez entreq 1 et 7");
         break;
